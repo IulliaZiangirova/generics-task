36bf068e161822df3416e4d87f3992f8c94a7da0
Зиангирова Юлия Руслановна <y.ziangirova@hq.qiwi.com>
2023-10-27 17:24:03 +0300

    MyList is implemented

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,58 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+public class MyList <T extends Number>{
+
+  private int defaultSize = 10;
+  private int countOfElements = 0;
+  private T [] array = new T [defaultSize];
+
+
+
+  public void add(T t) {
+    if (countOfElements >= array.length / 2){
+      resize();
+    }
+    array[countOfElements] = t;
+    countOfElements++;
+
   }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
+  public T get(int index) {
+    if (index < array.length && index >= 0){
+      return array[index];
+    }else throw new ArrayIndexOutOfBoundsException();
+
   }
 
   private void resize() {
-    throw new RuntimeException("Not implemented");
+    int newSize = (int) (array.length * 1.5);
+    array = Arrays.copyOf(array, newSize);
   }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
+  public T remove(int index) {
+    if (index < array.length && index >= 0){
+      T elementForRemoving = array[index];
+      for (int i = index; i < countOfElements; i++) {
+        array[i] = array[i + 1];
+      }
+      countOfElements--;
+      return elementForRemoving;
+    }else throw new ArrayIndexOutOfBoundsException();
   }
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
+  public <R extends Number> MyList<R> map(Function <T, R> f) {
+    MyList<R> myList = new MyList<>();
+    for (int i = 0; i < countOfElements; i++) {
+      myList.add(f.apply(this.get(i)));
+    }
+    return myList;
   }
 
   public int size() {
-    throw new RuntimeException("Not implemented");
+    return countOfElements;
   }
 }
